572954641b9d359e15682b39822134fe080ade2a
Anonim Education <soraplay111@gmail.com>
2021-08-04 17:56:44 +0300

    Добавьте файлы проекта.

---
+++ Таймеры/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Таймеры
+{
+    class Program
+    {
+        static Timer timer;
+
+        static void Main(string[] args)
+        {
+            TimerCallback tc = new TimerCallback(Print);
+
+            //timer = new Timer(tc, 5, 1000, 1000);
+
+            TimerCallback tcObj = new TimerCallback(PrintObject);
+            timer = new Timer(tcObj, new Point {X = 10, Y = 20 }, 1000, 1000);
+
+            Console.Read();
+        }
+
+        public static void Print(object obj)
+        {
+            Random rnd = new Random();
+
+            int n = rnd.Next(1, 5);
+            Console.WriteLine(n);
+            if (n == 3) timer.Dispose();
+        }
+
+        public static void PrintObject(object obj)
+        {
+            Point p = (Point)obj;
+            Console.WriteLine(p.X + " " + p.Y);
+        }
+    }
+
+    class Point
+    {
+        public int X;
+        public int Y;
+    }
+}
